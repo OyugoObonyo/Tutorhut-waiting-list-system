96f68ee379f90b68c04674281e00b9bdb3099415
OyugoObonyo <nyikal1999@gmail.com>
2022-02-11 17:23:04 +0300

    Integrate flask email extension to app

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask, flash, redirect, render_template, request, url_for
+import email
+from flask import Flask, flash, redirect, render_template, url_for
 from config import Config
 import requests
+from flask_mail import Mail, Message
+from flask_wtf import FlaskForm
+from wtforms.validators import Email, DataRequired
+from wtforms import StringField, SubmitField
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+mail = Mail(app)
 
 
 @app.route('/')
@@ -32,11 +38,25 @@ def update_sheets(email):
     response = requests.post(url=url, json=data, headers=headers)
 
 
-def send_email(email):
+def send_email(subject, sender, recipient, body):
     """
-    A function that se
+    A function that sends a message confirming subscription to waitlist
+    @subject: The email's subject
+    @sender: The sender's email
+    @recipient: The email of user signing up on waitlist
+    @body: The email's body.
     """
-    pass
+    msg = Message(subject=subject, sender=sender, recipients=recipient)
+    msg.html = body
+    mail.send(msg)
+
+
+class EmailSignUpForm(FlaskForm):
+    """
+    A class representing the sign up to waitlist form
+    """
+    email = StringField(validators=[DataRequired(), Email()])
+    submit = SubmitField('Get Early Access')
 
 
 @app.route('/join-list')
@@ -45,15 +65,19 @@ def join_list():
     Method that appends users to the waiting list
     Users get an email confirming their registration after being added to list
     """
-    # get email address from form
-    email = request.form.get('email')
-    # ensure email
-    if email is None:
+    form = EmailSignUpForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        update_sheets(email)
+        # <<<<<<add a flash message in case user tries to re-register
+        flash('You have succesfully subscribed to our waiting list', 'success')
+        send_email(
+            subject="Tutorhut waiting list subscription",
+            sender=app.config['MAIL_USERNAME'],
+            recipient=email,
+            body=render_template('email/email.html')
+        )
         return redirect(url_for('index'))
-    update_sheets(email)
-    # add a flash message in case user tries to re-register
-    send_email(email)
-    flash('You have succesfully subscribed to our waiting list', 'success')
     return redirect(url_for('index'))
 
 
