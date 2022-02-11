ed369428ab50a4ee91ede971578ab1000df4b741
OyugoObonyo <nyikal1999@gmail.com>
2022-02-11 14:52:23 +0300

    create initial routes

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from config import Config
+import requests
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 @app.route('/')
 def index():
@@ -8,6 +11,31 @@ def index():
     Renders the application's home page
     """
     return render_template('index.html')
+
+
+def update_sheets():
+    """
+    Documentation
+    """
+    pass
+
+
+def send_email(email):
+    """
+    A function that se
+    """
+    pass
+
+
+@app.route('/join-list')
+def join_list():
+    """
+    Method that appends users to the waiting list
+    Users get an email confirming their registration after being added to list
+    """
+    # get email address from form
+    email = request.form.get('email')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
